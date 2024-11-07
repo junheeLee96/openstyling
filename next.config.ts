2c677e0dfb6db936523586cc451ff21c32335089
junheeLee96 <dlwnsgml203@naver.com>
2024-11-07 21:42:36 +0900

    remote patterns > next.config

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,34 @@ const isProd = process.env.NODE_ENV === "production";
 const repository = "https://github.com/junheeLee96/openstyling";
 
 const nextConfig: NextConfig = {
-  // output: "export",
+  // output: "export",\
   images: {
-    unoptimized: true,
-    domains: [
-      "openweathermap.org",
-      "shopping-phinf.pstatic.net",
-      "oaidalleapiprodscus.blob.core.windows.net",
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        port: "",
+        pathname: "**",
+      },
     ],
+    // domains: [
+    //   "openweathermap.org",
+    //   "shopping-phinf.pstatic.net",
+    //   "oaidalleapiprodscus.blob.core.windows.net",
+    // ],
   },
 };
 
