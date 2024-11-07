49bb1a76950cb6e4dd5568ac4cab86211c4795e8
junheeLee96 <dlwnsgml203@naver.com>
2024-11-07 21:27:59 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 const repository = "https://github.com/junheeLee96/openstyling";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   images: {
     unoptimized: true,
     domains: [
